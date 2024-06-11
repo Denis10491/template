446e5e5827cf2974b6763edf50a38f16f149caed
Divrun <denis.03.ermakov@gmail.com>
2024-06-11 09:32:20 +0500

    feat(create.sh): added functionality for creating a repository via the console.

--- create.sh
+++ create.sh
@@ -3,8 +3,7 @@ NAME=$2
 NAME=$(echo "$NAME" | tr '[:lower:]' '[:upper:]' | head -c1)$(echo "$NAME" | tr '[:upper:]' '[:lower:]' | tail -c+2)
 
 if [[ $SUBJECT -eq 'controller' ]]; then
-echo '
-<?php
+echo '<?php
 
 declare(strict_types=1);
 
@@ -14,4 +13,19 @@ class '${NAME}'Controller
 {
 }
 ' > app/controllers/${NAME}Controller.php
+fi
+
+if [[ $SUBJECT -eq 'repository' ]]; then
+echo '<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use Core\Repository\Repository;
+
+class '${NAME}'Repository extends Repository
+{
+}
+' > app/repositories/${NAME}Repository.php
 fi
